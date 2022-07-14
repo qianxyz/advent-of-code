6eaa7fa5d56f0f34a61419c5c3041a50f9f1c61f
Qianxy <xyqian67@gmail.com>
2022-07-14 09:50:18 +0800

    Finish 2021 4

--- 2021/4.py
+++ 2021/4.py
@@ -10,12 +10,48 @@ class Board:
     def __init__(self, grid: list[list[int]]) -> None:
         self.grid = grid
 
+    def mark(self, n: int):
+        changes = []
+        for i in range(5):
+            for j in range(5):
+                if self.grid[i][j] == n:
+                    self.grid[i][j] = -1
+                    changes.append((i, j))
+        return changes
+
+    def is_win(self, changes: list[tuple[int, int]]):
+        for i, j in changes:
+            if self.grid[i] == [-1] * 5:
+                return True
+            if [self.grid[ii][j] for ii in range(5)] == [-1] * 5:
+                return True
+        return False
+
+    def score(self):
+        return sum(self.grid[i][j] for i in range(5) for j in range(5)
+                   if self.grid[i][j] > 0)
+
 
 def part1(raw_input: list[str]):
-    _ = _parse_input(raw_input)
-    return NotImplemented
+    draw, grids = _parse_input(raw_input)
+    boards = [Board(grid) for grid in grids]
+    for n in draw:
+        for board in boards:
+            if board.is_win(board.mark(n)):
+                return board.score() * n
 
 
 def part2(raw_input: list[str]):
-    _ = _parse_input(raw_input)
-    return NotImplemented
+    draw, grids = _parse_input(raw_input)
+    boards = [Board(grid) for grid in grids]
+    last_score = 0
+    for n in draw:
+        boards_left = []
+        for board in boards:
+            if board.is_win(board.mark(n)):
+                last_score = board.score()
+            else:
+                boards_left.append(board)
+        boards = boards_left
+        if not boards:
+            return last_score * n
