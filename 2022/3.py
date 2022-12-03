20e88b8015f8b3d3df87e32b318dc244198ce33f
Qianxy <xyqian67@gmail.com>
2022-12-03 13:34:51 +0800

    Rusty day 3

---
+++ 2022/3.py
@@ -0,0 +1,29 @@
+def _parse_input(raw_input: list[str]):
+    return [line for line in raw_input]
+
+
+def priority(c):
+    if 'a' <= c <= 'z':
+        return ord(c) - ord('a') + 1
+    elif 'A' <= c <= 'Z':
+        return ord(c) - ord('A') + 27
+    else:
+        raise ValueError
+
+
+def part1(raw_input: list[str]):
+    input = _parse_input(raw_input)
+    result = 0
+    for line in input:
+        l1 = line[:len(line) // 2]
+        l2 = line[len(line) // 2:]
+        result += priority(set(l1).intersection(l2).pop())
+    return result
+
+
+def part2(raw_input: list[str]):
+    input = _parse_input(raw_input)
+    return sum(
+        priority(set(l1).intersection(l2).intersection(l3).pop())
+        for (l1, l2, l3) in [input[i:i+3] for i in range(0, len(input), 3)]
+    )
